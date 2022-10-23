a3f6b667be70a7251091f376927cab6b4224140e
ItzInten <bpridatko@gmail.com>
2022-10-23 22:20:54 +0100

    adding cars

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
-Console.ReadKey();
+﻿using System;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            carsappearing();
+            Console.ReadKey();
+        }
+        
+        static int carsappearing()
+        {
+            int cars = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                var rand = new Random();
+                int time = rand.Next(1500, 2200);
+                Thread.Sleep(time);
+                Console.WriteLine("Car created");
+                int queue = 0;
+                queue++;
+                cars ++;
+            }
+            return cars;
+        }
+    }
+}
