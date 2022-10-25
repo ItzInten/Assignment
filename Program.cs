178be664174c183c6ea036e28c3d76873c8113f2
ItzInten <bpridatko@gmail.com>
2022-10-25 23:26:23 +0100

    added vehicleappearing

--- Program.cs
+++ Program.cs
@@ -4,27 +4,67 @@ namespace Assignment
 {
     internal class Program
     {
-        
+
         static void Main(string[] args)
         {
-            carsappearing();
+            vehicle vehicle1 = new vehicle();
+            Console.WriteLine(vehicle1.vehicleappearing());
             Console.ReadKey();
         }
-        
-        static int carsappearing()
+    }
+    class vehicle
+    {
+        int infinity, queue, vehicleamount = 0;
+        public int vehicleappearing()
         {
-            int cars = 0;
-            for (int i = 0; i < 100; i++)
+            while (infinity == 0)
             {
                 var rand = new Random();
-                int time = rand.Next(1500, 2200);
-                Thread.Sleep(time);
-                Console.WriteLine("Car created");
-                int queue = 0;
+
+                if (vehicleamount <4)
+                {
+                    int time = rand.Next(1500, 2200);
+                    Thread.Sleep(time);
+                    vehicleamount++;
+                    Console.WriteLine("Vehicle created");
+                }
+                else 
+                {
+                    int time = rand.Next(1000, 2000);
+                    Thread.Sleep(time);
+                    vehicleamount++;
+                    Console.WriteLine("Vehicle created, but queue now is " + vehicleamount);
+                }
                 queue++;
-                cars ++;
+                int[,] array = new int[1, 4];
+                for (int i = 0; i < 1; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        if (j==1)
+                        {
+                            array[i,j] = rand.Next(1,26);
+                            Console.WriteLine(array[i, j]);
+                        }
+                        else if (j == 2)
+                        {
+                            array[i, j] = rand.Next(1,41);
+                            Console.WriteLine(array[i, j]);
+                        }
+                        else if (j == 3)
+                        {
+                            array[i, j] = rand.Next(1,76);
+                            Console.WriteLine(array[i, j]);
+                        }
+                        else
+                        {
+                            array[i, j] = rand.Next(1,4);
+                            Console.WriteLine(array[i, j]);
+                        }
+                    }
+                }
             }
-            return cars;
+            return vehicleamount;
         }
     }
 }
