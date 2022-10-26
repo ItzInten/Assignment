60486e55649e1f9c9412b5c390b668a1330b62b7
ItzInten <bpridatko@gmail.com>
2022-10-26 17:51:52 +0100

    generate 3 types of vehicles and tank capacity

--- vehicle.cs
+++ vehicle.cs
@@ -1,61 +1,52 @@
 class vehicle
 {
-    int infinity, queue, vehicleamount = 0;
-    int maxamountofvehicles = 0;
+        int[,] array = new int[100, 2];
     public int vehicleappearing()
-    {
-        while (maxamountofvehicles < 100)
-        {
-            var rand = new Random();
 
-            if (vehicleamount < 4)
+    {
+        var rand = new Random();
+        for (int i = 0; i < 100; i++)
+        {
+            if (i <= 3)
             {
                 int time = rand.Next(1500, 2200);
                 Thread.Sleep(time);
-                vehicleamount++;
                 Console.WriteLine("Vehicle created");
             }
             else
             {
                 int time = rand.Next(1000, 2000);
                 Thread.Sleep(time);
-                vehicleamount++;
-                Console.WriteLine("Vehicle created, but queue now is " + vehicleamount);
+                Console.WriteLine("Vehicle created, but queue now is " + (i+1));
             }
-            int[,] arrayofarrays = new int[100, 2];
-
-            int[,] array = new int[1, 4];
-            for (int i = 0; i < 1; i++)
+            for (int j = 0; j < 2; j++)
             {
-                for (int j = 0; j < 4; j++)
+                if (j == 0)
                 {
-                    if (i == 0 && j == 0)
-                    {
-                        array[i, j] = rand.Next(1, 4);
-                        Console.WriteLine(array[i, j]);
-                    }
-                    else if (array[0, 0] == 1)
-                    {
-                        array[i, j] = rand.Next(1, 26);
-                        Console.WriteLine(array[i, j]);
-                        break;
-                    }
-                    else if (array[0, 0] == 2)
-                    {
-                        array[i, j] = rand.Next(1, 41);
-                        Console.WriteLine(array[i, j]);
-                        break;
-                    }
-                    else if (array[0, 0] == 3)
-                    {
-                        array[i, j] = rand.Next(1, 76);
-                        Console.WriteLine(array[i, j]);
-                        break;
-                    }
+                    array[i, j] = rand.Next(1, 4);
+                    Console.WriteLine(array[i, j]);
+                }
+                else if (array[i, 0] == 1)
+                {
+                    array[i, j] = rand.Next(1, 26);
+                    Console.WriteLine(array[i, j]);
+                    break;
+                }
+                else if (array[i, 0] == 2)
+                {
+                    array[i, j] = rand.Next(1, 41);
+                    Console.WriteLine(array[i, j]);
+                    break;
+                }
+                else if (array[i, 0] == 3)
+                {
+                    array[i, j] = rand.Next(1, 76);
+                    Console.WriteLine(array[i, j]);
+                    break;
                 }
             }
-            maxamountofvehicles++;
         }
-        return vehicleamount;
+        Console.ReadKey();
+        return array[100,2];
     }
 }
